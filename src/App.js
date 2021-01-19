533c6d9ad13f703813d2f55f1dfc329ae19dcdee
Carlos Sivira <carlos.csivira@gmail.com>
2021-01-19 18:35:33 -0400

    Fetching data and update value done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios';
 
 const App = () => {
+  const [name, setName] = useState('');
+  const [gender, setGender] = useState('');
+
+  const handleGender = (nameString) => {
+    axios.get('https://api.genderize.io?name=' + name)
+    .then(response => {
+      setGender(JSON.stringify(response));
+    }).catch(e => setGender(JSON.stringify(e)))
+  };
+
   return (
-    <div>Hola Mundo</div>
+    <div>
+      <input type='text' value={name} onChange={(e) => setName(e.target.value) }/>
+      <button onClick={() => handleGender(name)}>Get Gender</button>
+      <p>{gender}</p>
+    </div>
   );
 }
 
